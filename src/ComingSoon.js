8db3c7944d650a70470f386daf3cdd95d3fec2b9
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-11 03:08:17 +0530

    UI Fixed

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./ComingSoon.css";
-import logo from "./assets/slotify-full-1.png";
-import instagram from "./assets/instagram.png";
-import email from "./assets/email1.png";
-import linkedin from "./assets/linkedin.png";
-import footer from "./assets/Untitled.png";
+import logo from "./assets/slotify-full.png";
+// import instagram from "./assets/instagram.png";
+// import email from "./assets/email1.png";
+// import linkedin from "./assets/linkedin.png";
+// import footer from "./assets/Untitled.png";
 
 const ComingSoon = () => {
   return (
-    <>
-    <div className="coming-soon-container">
+    <div className="content">
+    {/* <div className="coming-soon-container">
       <img src={logo} alt="Logo" className="logo"/>
       <p className="message">Coming Soon...</p>
     </div>
@@ -26,8 +26,40 @@ const ComingSoon = () => {
           <img src={instagram} alt="Instagram" className="mediaIcon"/>
         </a>
       </div>
+    </div> */}
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-dark bg-transparenet">
+                <a class="navbar-brand" href="#">
+                  <img src={logo} alt="Logo" className="logo"/>
+                </a>
+                <span class="navbar-text d-none d-sm-inline-block">connect@slotify.live</span>
+            </nav>
+        </div>
+    </header>
+    <main class="my-auto">
+        <div class="container1">
+            <h1 class="page-title">We're coming soon</h1>
+            <p>Smart Booking: Say goodbye to hassles with our intelligent booking system.
+                Personalized Recommendations: Tailored suggestions based on your preferences.</p>
+            <p>Stay connected</p>
+            <nav class="footer-social-links">
+    
+                <a href="https://www.linkedin.com/company/slotify-live" class="social-link" target="_blank">
+                  <i class="mdi mdi-linkedin"></i></a>
+
+                <a href="https://www.instagram.com/slotify.live/" class="social-link" target="_blank">
+                  <i class="mdi mdi-instagram"></i></a>
+
+                <a href="https://twitter.com/slotify_live" class="social-link" target="_blank">
+                  <i class="mdi mdi-twitter"></i></a>
+
+                <a href="mailto:connect@slotify.live" class="social-link" target="_blank">
+                  <i class="mdi mdi-email"></i></a>
+            </nav>
+        </div>
+    </main>
     </div>
-    </>
   );
 };
 
